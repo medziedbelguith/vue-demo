b96a39d3fa96a7ea937bacc8955b22a4523fbc52
medziedbelguith <medziedbelguith@gmail.com>
2022-07-29 20:15:29 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+<div>
+<Header @changeTitle="changeText" :text="text"/>
+</div>
+</template>
+
+<script>
+import Header from "@/components/Header.vue"
+
+export default {
+  name: "App",
+  components:{
+    Header,
+  },
+  data(){
+    return{
+    text:"Ini adalah text dari app.vue"
+    };
+  },
+  methods:{
+    changeText(newText){
+      this.text=newText;
+    }
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+<div>
+<h2>{{ text }}</h2>
+<button @click="changeText">Change text</button>
+</div>
+</template>
+
+<script>
+export default {
+name:"Header",
+props:["text"],
+data(){
+    return{
+     newText:"Ini adalah text dari header.vue",
+    };
+},
+methods:{
+    changeText(){
+        this.$emit("changeTitle",this.newText);
+    },
+},
+};
+</script>
+
+<style>
+</style>
